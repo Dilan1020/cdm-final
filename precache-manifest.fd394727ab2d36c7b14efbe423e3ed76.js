75d74b1f94845c2f8bc27199fb3ef03aba4c8cc4
Dilan Ozkaynak <dilanjames10@gmail.com>
2020-04-08 18:57:41 -0400

    Updates

---
+++ precache-manifest.fd394727ab2d36c7b14efbe423e3ed76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b420cf77b19e376d7fb2d8c962263dc",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "fd4f91358a26039964c0",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c1d2936f3d79d3bc2eb",
+    "url": "/react-gh-pages/static/js/2.bf82200c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.bf82200c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd4f91358a26039964c0",
+    "url": "/react-gh-pages/static/js/main.526f43c0.chunk.js"
+  },
+  {
+    "revision": "d6c2721c0cc86d0de5ec",
+    "url": "/react-gh-pages/static/js/runtime-main.a4cbb2e0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
